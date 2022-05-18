b29c589fa2670cd9b332963f2bc922a2ccc239b5
Lucas <lucasmarchd01@gmail.com>
2022-05-18 12:02:12 -0400

    created file to set up csv file for video

---
+++ PrepareData.py
@@ -0,0 +1,22 @@
+# Creates csv file for folder with video images
+
+
+import os
+import csv
+
+
+
+directory = "C:\\Users\\Lucas March\\Video Images\\GH010067"
+videoName = "GH010067"
+
+with open(directory + "\\" + videoName + ".csv", "w", newline="") as f:
+    writer = csv.writer(f)
+    writer.writerow(['fileName', 'Time Recorded', 'Phase', 'Folder'])
+
+    for image in os.listdir(directory):
+        if image.endswith('.jpg'):
+            imageSplit = image.removesuffix(".jpg")
+            imageSplit = imageSplit.split("@t=")
+            writer.writerow([image, imageSplit[-1], None, directory])
+    print(videoName + ".csv saved to: " + directory)
+
