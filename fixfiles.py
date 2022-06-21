28beaa5156f0868b3e356a0625e99ea17ada2d4f
Lucas <lucasmarchd01@gmail.com>
2022-06-20 20:59:44 -0400

    add empty text files

--- fixfiles.py
+++ fixfiles.py
@@ -2,19 +2,34 @@ import os
 from natsort import natsorted
 import pandas as pd
 
-videofile = "GH010064"
-filepath = "C:\\Users\\Lucas March\\Video Images\\Training\\GH010064"
 
-os.chdir(filepath)
-data = pd.read_csv("GH010064_Labels.csv")
+def fun1():
+    videofile = "GH010064"
+    filepath = "C:\\Users\\Lucas March\\Video Images\\Training\\GH010064"
 
-time = 0
+    os.chdir(filepath)
+    data = pd.read_csv("GH010064_Labels.csv")
 
-for image in natsorted(os.listdir(filepath)):
-    if os.path.exists(image) and image.endswith(".jpg"):
-        print(videofile + "@t=%.3f.jpg" % time)
-        os.rename("GH010059@t=%.3f.jpg" % time, videofile + "@t=%.3f.jpg" % time)
-    data["fileName"] = data["fileName"].replace(["GH010059@t=%.3f.jpg" % time], 
-                                               videofile + "@t=%.3f.jpg" % time)
-    time += 1/15
-data.to_csv("GH010064444_Labels.csv", index=False)
+    time = 0
+
+    for image in natsorted(os.listdir(filepath)):
+        if os.path.exists(image) and image.endswith(".jpg"):
+            print(videofile + "@t=%.3f.jpg" % time)
+            os.rename("GH010059@t=%.3f.jpg" % time, videofile + "@t=%.3f.jpg" % time)
+        data["fileName"] = data["fileName"].replace(["GH010059@t=%.3f.jpg" % time], 
+                                                videofile + "@t=%.3f.jpg" % time)
+        time += 1/15
+    data.to_csv("GH010064444_Labels.csv", index=False)
+
+def fun2():
+    labelspath = "C:\\Users\\Lucas\\OneDrive - Queen's University\\Summer Research 2022\\yolov5\\runs\\detect\\gh010054_detect\\labels\\"
+    imagespath = "C:\\Users\\Lucas\\Video Images\\Training\\GH010054"
+    os.chdir(imagespath)
+    for image in natsorted(os.listdir(imagespath)):
+        if not os.path.exists(labelspath + image.replace(".jpg", ".txt")) and image.endswith(".jpg"):
+            print("adding file: " + image.replace(".jpg", ".txt"))
+            f = open(labelspath + image.replace(".jpg", ".txt"), 'a')
+            f.close()
+
+if __name__ == "__main__":
+    fun2()
